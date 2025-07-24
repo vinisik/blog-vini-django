e16501279d918860ae9c8dfddbdfb19add683baf
vinisik <siqueiravini29@gmail.com>
2025-07-24 13:59:25 -0300

    Editando html e css

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,9 +1,11 @@
+from blog.views import index, page, post
 from django.urls import path
-from blog.views import index
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('post/', post, name='post'),
+    path('page/', page, name='page'),
 ]
 
