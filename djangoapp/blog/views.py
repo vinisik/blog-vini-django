0ad94d93ed216de32c75747fa4e7d24d26bd236c
vinisik <siqueiravini29@gmail.com>
2025-07-25 22:42:09 -0300

    Melhora a estrutura do CSS e HTML, adiciona título dinâmico nas páginas e implementa tratamento de erros para usuários e posts não encontrados.

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -2,6 +2,8 @@ from blog.models import Post, Page
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http import Http404
 
 PER_PAGE = 9
 
@@ -24,8 +26,19 @@ def index(request):
 
 # Posts por autor
 def created_by(request, author_pk):
+    user = User.objects.filter(pk=author_pk).first()
+
+    if user is None:
+        raise Http404()
+
     posts = Post.objects.filter(is_published=True)\
         .filter(created_by__pk=author_pk)
+    user_full_name = user.username
+
+    if user.first_name:
+        user_full_name = f'{user.first_name} {user.last_name}'
+    page_title = 'Posts de ' + user_full_name + ' - '
+    page_title_main = 'Posts de ' + user_full_name
 
     paginator = Paginator(posts, PER_PAGE)
     page_number = request.GET.get("page")
@@ -36,8 +49,11 @@ def created_by(request, author_pk):
         'blog/pages/index.html',
         {
             'page_obj': page_obj,
+            'page_title': page_title,
+            'page_title_main': page_title_main,
         }
     )
+
 
 
 # Posts por categoria
@@ -49,11 +65,19 @@ def category(request, slug):
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
+    if len(page_obj) == 0:
+        raise Http404()
+
+    page_title = f'{page_obj[0].category.name} - Categoria - '
+    page_title_main = f'Categorias > {page_obj[0].category.name}'
+
     return render(
         request,
         'blog/pages/index.html',
         {
             'page_obj': page_obj,
+            'page_title': page_title,
+            'page_title_main': page_title_main,
         }
     )
 
@@ -67,11 +91,19 @@ def tag(request, slug):
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
+    if len(page_obj) == 0:
+        raise Http404()
+
+    page_title = f'{page_obj[0].tags.first().name} - Tag - '
+    page_title_main = f'Tags > {page_obj[0].tags.first().name}'
+
     return render(
         request,
         'blog/pages/index.html',
         {
             'page_obj': page_obj,
+            'page_title': page_title,
+            'page_title_main': page_title_main,
         }
     )
 
@@ -101,34 +133,46 @@ def search(request):
 
 # Páginas estáticas
 def page(request, slug):
-    page = (
+    page_obj = (
         Page.objects
         .filter(is_published=True)
         .filter(slug=slug)
         .first()
     )
 
+    if page_obj is None:
+        raise Http404()
+
+    page_title = f'{page_obj.title} - '
+
     return render(
         request,
         'blog/pages/page.html',
         {
-            'page': page,
+            'page': page_obj,
+            'page_title': page_title,
         }
     )
 
 
-
 # Conteúdo do post
 def post(request, slug):
-    post = (Post.objects.filter(is_published=True)
-            .filter(slug=slug)
-            .first()
+    post_obj = (
+        Post.objects.filter(is_published=True)
+        .filter(slug=slug)
+        .first()
     )
+
+    if post_obj is None:
+        raise Http404()
+
+    page_title = f'{post_obj.title} - '
 
     return render(
         request,
         'blog/pages/post.html',
         {
-            'post': post,
+            'post': post_obj,
+            'page_title': page_title,
         }
     )
